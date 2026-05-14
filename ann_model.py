2301ef1233df9e0c6d406afee991606b7c05ba10
seurrt <saioaelizondou@gmail.com>
2026-05-14 09:53:12 +0200

    Added ANN model

---
+++ ann_model.py
@@ -0,0 +1,74 @@
+import sys
+#sys.path.append('../')
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+from torch.nn import Linear, ReLU, CrossEntropyLoss, Sequential, Conv2d, MaxPool2d, Module, Softmax, BatchNorm2d, \
+    Dropout
+from torch.optim import Adam, SGD
+from copy import deepcopy
+
+# Sources: 
+# https://media.geeksforgeeks.org/wp-content/uploads/20250617160652239500/NNwith_early_stopping.ipynb
+# https://docs.pytorch.org/tutorials/beginner/saving_loading_models.html#save-load-state-dict-recommended
+
+class EarlyStopping:
+    def __init__(self, patience=5, delta=0, device = "cpu"):
+        self.patience = patience
+        self.delta = delta
+        self.best_score = None
+        self.early_stop = False
+        self.counter = 0
+        self.best_model_state = None,
+        self.device = device
+
+    def __call__(self, val_loss, model):
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.best_model_state = deepcopy(model.cpu().state_dict())
+            model.to(self.device)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.best_model_state = deepcopy(model.cpu().state_dict())
+            model.to(self.device)
+            self.counter = 0
+
+
+class ANN_Class(nn.Module):
+    def __init__(self, input_dim = 1000):
+        super(ANN_Class, self).__init__()
+
+        self.fc1 = nn.Linear(input_dim, 2048)  
+
+        self.fc2 = nn.Linear(2048, 1024)
+
+        self.fc3 = nn.Linear(1024, 300)
+
+        self.dropout = nn.Dropout(0.25)
+
+
+    def encode(self, x):
+
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.dropout(x)
+        x = self.fc3(x)
+        x = x.view(x.size(0), -1)
+
+        return x
+
+    def forward(self, x):
+        x = self.encode(x)
+        return x
---
+++ ann_model_massspecgym.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from massspecgym.models.retrieval.base import RetrievalMassSpecGymModel
+from massspecgym.models.base import Stage
+from ann_model import ANN_Class
+
+class AnnRetrieval(RetrievalMassSpecGymModel):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.ann = ANN_Class(input_dim=70000)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # x shape: (batch_size, 70000)
+        return self.ann(x)  # output: (batch_size, 300)
+
+    def step(self, batch: dict, stage: Stage) -> dict:
+        x = batch["spec"]
+        mol_true = batch["mol"]
+        mol_pred = self.forward(x)
+
+        loss = nn.functional.mse_loss(mol_pred, mol_true)
+
+        scores = None
+        if stage == Stage.TEST:
+            cands = batch["candidates_mol"]
+            batch_ptr = batch["batch_ptr"]
+            mol_pred_repeated = mol_pred.repeat_interleave(batch_ptr, dim=0)
+            scores = nn.functional.cosine_similarity(mol_pred_repeated, cands)
+
+        return dict(loss=loss, scores=scores)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.0001)
